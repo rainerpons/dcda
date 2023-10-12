9e85b64bbb149554a2d5395f8e9b2c4fbccda752
rainerpons <rainerpons11@gmail.com>
2023-10-11 23:09:19 -0700

    Replaced boilerplate code with a checkbox for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [checked, setChecked] = useState<boolean>(false);
+
+  const handleCheck = () => setChecked(checked => !checked);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Don't Care + Didn't Ask</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <input id="checkbox" type="checkbox" onChange={handleCheck} />
+        <label htmlFor="checkbox">{checked ? 'Is' : 'Isn\'t'} checked</label>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p className="footnote">
+        Meme app based around the <a href="https://www.reddit.com/r/copypasta/comments/qmxadi" target="_blank">"don't care didn't ask" copypasta</a>
       </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
