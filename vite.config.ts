09b0d8eae8480d6822ff2733a9d94c8d5af93afa
rainerpons <rainerpons11@gmail.com>
2023-10-22 18:27:21 -0700

    One last update to base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'rainerpons.github.io/dcda/',
+  base: 'rainerpons.github.io/dcda',
 });
